5632156a51f0409040bd23519065dd7a56fa0fc8
Blake <blake.wilson@utdallas.edu>
2017-04-27 12:16:20 -0500

    added in lines to test_input_options to test the new options for adding analyses.

--- tests/test_bilayer_analyzer.py
+++ tests/test_bilayer_analyzer.py
@@ -5,16 +5,30 @@ from vorbilt.bilayer_analyzer.analysis_protocols import valid_analysis
 
 #input options
 def test_input_options():
+    #initialize analyzer with keyword options--and default analysis
     sel_string = "not resname CLA and not resname TIP3 and not resname POT"
     ba = BilayerAnalyzer(
         psf_file='../vorbilt/sample_bilayer/sample_bilayer.psf',
         trajectory='../vorbilt/sample_bilayer/sample_bilayer_10frames.dcd',
         selection=sel_string,
     )
+    #add analysis by string input
     ba.add_analysis('msd msd_2 leaflet upper resname POPC')
+    #add plot by string -- currenlty only available
     ba.add_plot('msd msd_p msd_1 DOPE-U msd_2 POPC-U')
+    #add analysis by list/tuple input
+    ba.add_analysis(['msd', 'msd_3', {'resname': 'POPC', 'leaflet': 'lower'}])
+    #add analysis by dictionary input
+    analysis_in_dict = dict()
+    analysis_in_dict['analysis_key'] = 'msd'
+    analysis_in_dict['analysis_id'] = 'msd_4'
+    analysis_in_dict['analysis_settings'] = {'resname': 'DOPE', 'leaflet': 'lower'}
+    ba.add_analysis(analysis_in_dict)
+    # run the analyses
     ba.run_analysis()
+    #initialize analyzer using input script
     ba = BilayerAnalyzer(input_file='sample_1.in')
+    #run analysis
     ba.run_analysis()
 
 #analsyses with default options
@@ -30,4 +44,12 @@ def test_analysis_defaults():
         ba.add_analysis(analysis_in)
     ba.print_analysis_protocol()
     ba.run_analysis()
+    print("outputs:")
+    for key in valid_analysis:
+        a_id = key+"_t"
+        print(a_id+":")
+        print(ba.get_analysis_data(a_id))
 
+
+test_input_options()
+test_analysis_defaults()
