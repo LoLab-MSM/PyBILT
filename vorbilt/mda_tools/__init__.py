58e56f11e2b88e27aa9cfc1301743cdcfd514f71
Blake <blakeaw1102@gmail.com>
2017-06-11 15:54:37 -0500

    updated __init__.py

--- vorbilt/mda_tools/__init__.py
+++ vorbilt/mda_tools/__init__.py
@@ -1,4 +1,9 @@
-from mda_area_compressibility_modulus import area_compressibility_modulus 
+"""
+The mda_tools module contains functions that operate on MDAnalysis objects
+to perform standalone analyses, as well as other operations. 
+"""
+
+from mda_area_compressibility_modulus import area_compressibility_modulus
 from mda_density_profile import electron_density_profile, electron_density_profile_gaussians, mass_density_profile
 from mda_deuterium_order_parameter import average_deuterium_order_Moore, deuterium_order_Moore, average_deuterium_order_Vermeer
 from mda_msd import mda_msd as msd
