130ba3ad0b832b18c3be46887c84b8b069220e28
Blake <blake.wilson@utdallas.edu>
2017-07-25 12:51:12 -0500

    started analysis_modules with msd_diffusion module; analysis modules are sets of analyses and plotting to generate data for specific types of analysis

---
+++ pybilt/bilayer_analyzer/analysis_modules.py
@@ -0,0 +1,211 @@
+import numpy as np
+
+from pybilt.bilayer_analyzer.bilayer_analyzer import BilayerAnalyzer
+from pybilt.mda_tools import diffusion_coefficients as dc
+
+
+
+
+def msd_diffusion(structure_file, trajectory_file, selection_string, resnames=[], frame_start=0, frame_end=-1, frame_interval=1, dump_path=None):
+
+    analyzer = BilayerAnalyzer(structure=structure_file,
+                               trajectory=trajectory_file,
+                               selection=selection_string)
+
+    analyzer.set_frame_range(frame_start, frame_end, frame_interval)
+    #for each lipid resname in the input resnames add an msd analysis for both, upper, and lower leaflets
+    for lipid_type in resnames:
+        a_name = "msd_"+lipid_type
+        add_string = "msd "+a_name+" resname "+lipid_type
+        add_string_upper = "msd "+a_name+"_upper resname "+lipid_type+" leaflet upper"
+        add_string_lower = "msd "+a_name+"_lower resname "+lipid_type+" leaflet lower"
+        analyzer.add_analysis(add_string)
+        analyzer.add_analysis(add_string_upper)
+        analyzer.add_analysis(add_string_lower)
+
+    #now add plot protocols
+    analyzer.add_plot('msd msd_a msd_1 All')
+    # plot with each of the lipid types on same plot (leaflet=both)
+    l_msd_string = "msd msd_l"
+    for lipid_type in resnames:
+        l_msd_string+=" msd_"+lipid_type+" "+lipid_type
+    analyzer.add_plot(l_msd_string)
+
+    #plots for each lipid with upper, lower, both on same plot
+    for lipid_type in resnames:
+        add_string = "msd "+lipid_type+"_cul msd_"+lipid_type+" Composite msd_"+lipid_type+"_upper Upper msd_"+lipid_type+"_lower Lower"
+        analyzer.add_plot(add_string)
+
+    analyzer.print_analysis_protocol()
+    analyzer.print_plot_protocol()
+
+    #run the analyzer
+    analyzer.run_analysis()
+
+    #dump the output
+    analyzer.dump_data(path=dump_path)
+
+    #make MSD vs time plots
+    analyzer.save_all_plots()
+
+    #compute diffusion coefficients
+    #composite
+
+    msd_dat = analyzer.get_analysis_data('msd_1')
+
+    times = msd_dat[:,0]
+    msd_vals = msd_dat[:,1]
+    #diffusion coeff, whole range
+    # Simple application of Einstein relation
+    D_e = dc.diffusion_coefficient_Einstein(times, msd_vals)
+    # Use linear fit
+    D_l = dc.diffusion_coefficient_linear_fit(times, msd_vals)
+    # Use anomalous diffusion fit
+    D_a = dc.diffusion_coefficient_anomalous_fit(times, msd_vals)
+    print("Composite for all lipids and both leaflets; whole time range")
+    print("Values from estimators:")
+    print("  Basic Einstein relation:")
+    print("    Diffusion coefficient: {}".format(D_e))
+    print("  Linear fit:")
+    print("    Diffusion coefficient: {} Std Error: {}".format(D_l[0], D_l[1]))
+    print("  Anomalous diffusion fit:")
+    print("    Diffusion coefficient: {} Alpha value: {}".format(D_a[0], D_a[1]))
+    #diffusion coeff, first 10 ns
+    # Simple application of Einstein relation
+    D_e = dc.diffusion_coefficient_Einstein(times, msd_vals, time_range=[0.0, 10000.0])
+    # Use linear fit
+    D_l = dc.diffusion_coefficient_linear_fit(times, msd_vals, time_range=[0.0, 10000.0])
+    # Use anomalous diffusion fit
+    D_a = dc.diffusion_coefficient_anomalous_fit(times, msd_vals, time_range=[0.0, 10000.0])
+    print("Composite for all lipids and both leaflets; 0-10 ns")
+    print("Values from estimators:")
+    print("  Basic Einstein relation:")
+    print("    Diffusion coefficient: {}".format(D_e))
+    print("  Linear fit:")
+    print("    Diffusion coefficient: {} Std Error: {}".format(D_l[0], D_l[1]))
+    print("  Anomalous diffusion fit:")
+    print("    Diffusion coefficient: {} Alpha value: {}".format(D_a[0], D_a[1]))
+    #diffusion coeff,  10-100 ns
+    if max(times)>10000.0:
+        # Simple application of Einstein relation
+        D_e = dc.diffusion_coefficient_Einstein(times, msd_vals, time_range=[10000.0, 100000.0])
+        # Use linear fit
+        D_l = dc.diffusion_coefficient_linear_fit(times, msd_vals, time_range=[10000.0, 100000.0])
+        # Use anomalous diffusion fit
+        D_a = dc.diffusion_coefficient_anomalous_fit(times, msd_vals, time_range=[10000.0, 100000.0])
+        print("Composite for all lipids and both leaflets; 10-100 ns")
+        print("Values from estimators:")
+        print("  Basic Einstein relation:")
+        print("    Diffusion coefficient: {}".format(D_e))
+        print("  Linear fit:")
+        print("    Diffusion coefficient: {} Std Error: {}".format(D_l[0], D_l[1]))
+        print("  Anomalous diffusion fit:")
+        print("    Diffusion coefficient: {} Alpha value: {}".format(D_a[0], D_a[1]))
+
+    #now each individual lipid type
+    for lipid_type in resnames:
+        msd_dat = analyzer.get_analysis_data("msd_"+lipid_type)
+
+        times = msd_dat[:, 0]
+        msd_vals = msd_dat[:, 1]
+        # diffusion coeff, whole range
+        # Simple application of Einstein relation
+        D_e = dc.diffusion_coefficient_Einstein(times, msd_vals)
+        # Use linear fit
+        D_l = dc.diffusion_coefficient_linear_fit(times, msd_vals)
+        # Use anomalous diffusion fit
+        D_a = dc.diffusion_coefficient_anomalous_fit(times, msd_vals)
+        print("Composite for "+lipid_type+" and both leaflets; whole time range")
+        print("Values from estimators:")
+        print("  Basic Einstein relation:")
+        print("    Diffusion coefficient: {}".format(D_e))
+        print("  Linear fit:")
+        print("    Diffusion coefficient: {} Std Error: {}".format(D_l[0], D_l[1]))
+        print("  Anomalous diffusion fit:")
+        print("    Diffusion coefficient: {} Alpha value: {}".format(D_a[0], D_a[1]))
+        # diffusion coeff, first 10 ns
+        # Simple application of Einstein relation
+        D_e = dc.diffusion_coefficient_Einstein(times, msd_vals, time_range=[0.0, 10000.0])
+        # Use linear fit
+        D_l = dc.diffusion_coefficient_linear_fit(times, msd_vals, time_range=[0.0, 10000.0])
+        # Use anomalous diffusion fit
+        D_a = dc.diffusion_coefficient_anomalous_fit(times, msd_vals, time_range=[0.0, 10000.0])
+        print("Composite for "+lipid_type+" and both leaflets; 0-10 ns")
+        print("Values from estimators:")
+        print("  Basic Einstein relation:")
+        print("    Diffusion coefficient: {}".format(D_e))
+        print("  Linear fit:")
+        print("    Diffusion coefficient: {} Std Error: {}".format(D_l[0], D_l[1]))
+        print("  Anomalous diffusion fit:")
+        print("    Diffusion coefficient: {} Alpha value: {}".format(D_a[0], D_a[1]))
+        if max(times)>10000.0:
+            # diffusion coeff,  10-100 ns
+            # Simple application of Einstein relation
+            D_e = dc.diffusion_coefficient_Einstein(times, msd_vals, time_range=[10000.0, 100000.0])
+            # Use linear fit
+            D_l = dc.diffusion_coefficient_linear_fit(times, msd_vals, time_range=[10000.0, 100000.0])
+            # Use anomalous diffusion fit
+            D_a = dc.diffusion_coefficient_anomalous_fit(times, msd_vals, time_range=[10000.0, 100000.0])
+            print("Composite for "+lipid_type+" and both leaflets; 10-100 ns")
+            print("Values from estimators:")
+            print("  Basic Einstein relation:")
+            print("    Diffusion coefficient: {}".format(D_e))
+            print("  Linear fit:")
+            print("    Diffusion coefficient: {} Std Error: {}".format(D_l[0], D_l[1]))
+            print("  Anomalous diffusion fit:")
+            print("    Diffusion coefficient: {} Alpha value: {}".format(D_a[0], D_a[1]))
+        #now do individual leaflets
+        for leaflet in ['upper', 'lower']:
+            msd_dat = analyzer.get_analysis_data("msd_" + lipid_type+"_"+leaflet)
+
+            times = msd_dat[:, 0]
+            msd_vals = msd_dat[:, 1]
+            # diffusion coeff, whole range
+            # Simple application of Einstein relation
+            D_e = dc.diffusion_coefficient_Einstein(times, msd_vals)
+            # Use linear fit
+            D_l = dc.diffusion_coefficient_linear_fit(times, msd_vals)
+            # Use anomalous diffusion fit
+            D_a = dc.diffusion_coefficient_anomalous_fit(times, msd_vals)
+            print("Composite for " + lipid_type + " and "+leaflet+" leaflet; whole time range")
+            print("Values from estimators:")
+            print("  Basic Einstein relation:")
+            print("    Diffusion coefficient: {}".format(D_e))
+            print("  Linear fit:")
+            print("    Diffusion coefficient: {} Std Error: {}".format(D_l[0], D_l[1]))
+            print("  Anomalous diffusion fit:")
+            print("    Diffusion coefficient: {} Alpha value: {}".format(D_a[0], D_a[1]))
+            # diffusion coeff, first 10 ns
+            # Simple application of Einstein relation
+            D_e = dc.diffusion_coefficient_Einstein(times, msd_vals, time_range=[0.0, 10000.0])
+            # Use linear fit
+            D_l = dc.diffusion_coefficient_linear_fit(times, msd_vals, time_range=[0.0, 10000.0])
+            # Use anomalous diffusion fit
+            D_a = dc.diffusion_coefficient_anomalous_fit(times, msd_vals, time_range=[0.0, 10000.0])
+            print("Composite for " + lipid_type + " and "+leaflet+" leaflet; 0-10 ns")
+            print("Values from estimators:")
+            print("  Basic Einstein relation:")
+            print("    Diffusion coefficient: {}".format(D_e))
+            print("  Linear fit:")
+            print("    Diffusion coefficient: {} Std Error: {}".format(D_l[0], D_l[1]))
+            print("  Anomalous diffusion fit:")
+            print("    Diffusion coefficient: {} Alpha value: {}".format(D_a[0], D_a[1]))
+            if max(times) > 10000.0:
+                # diffusion coeff,  10-100 ns
+                # Simple application of Einstein relation
+                D_e = dc.diffusion_coefficient_Einstein(times, msd_vals, time_range=[10000.0, 100000.0])
+                # Use linear fit
+                D_l = dc.diffusion_coefficient_linear_fit(times, msd_vals, time_range=[10000.0, 100000.0])
+                # Use anomalous diffusion fit
+                D_a = dc.diffusion_coefficient_anomalous_fit(times, msd_vals, time_range=[10000.0, 100000.0])
+                print("Composite for " + lipid_type + " and "+leaflet+" leaflet; 10-100 ns")
+                print("Values from estimators:")
+                print("  Basic Einstein relation:")
+                print("    Diffusion coefficient: {}".format(D_e))
+                print("  Linear fit:")
+                print("    Diffusion coefficient: {} Std Error: {}".format(D_l[0], D_l[1]))
+                print("  Anomalous diffusion fit:")
+                print("    Diffusion coefficient: {} Alpha value: {}".format(D_a[0], D_a[1]))
+
+
+    return
---
+++ tests/test_analysis_module_msd_diffusion.py
@@ -0,0 +1,16 @@
+
+from pybilt.bilayer_analyzer.analysis_modules import msd_diffusion
+
+
+def test_analysis_module_msd_diffusion():
+    sel_string = "not resname CLA and not resname TIP3 and not resname POT"
+    msd_diffusion(structure_file='../pybilt/sample_bilayer/sample_bilayer.psf',
+                  trajectory_file='../pybilt/sample_bilayer/sample_bilayer_10frames.dcd',
+                  selection_string=sel_string,
+                  resnames=['POPC', 'DOPE'])
+
+    return
+
+if __name__ == '__main__':
+    test_analysis_module_msd_diffusion()
+
