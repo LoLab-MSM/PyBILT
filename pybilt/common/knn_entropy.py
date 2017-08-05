728524abe1034cf2b4caad5986661130c685cbbd
blakeaw <blakeaw1102@gmail.com>
2017-08-04 22:51:47 -0500

    fixed the style/smell items in knn_entropy noted by landscape.io

--- pybilt/common/knn_entropy.py
+++ pybilt/common/knn_entropy.py
@@ -62,7 +62,7 @@ def shannon_entropy(X, k=1, kth_dists=None):
     v_unit_ball = np.pi**(0.5*d)/gamma(0.5*d + 1.0)
     # log distances
     lr_k = np.log(r_k)
-    # Shannon entropy estimate  
+    # Shannon entropy estimate
     H = psi(n) - psi(k) + np.log(v_unit_ball) + (np.float(d)/np.float(n))*( lr_k.sum())
 
     return H
@@ -74,7 +74,7 @@ def shannon_entropy_pc(X, k=1, kth_dists=None):
     #     for Continuous Random Variables. Advances in Neural Information
     #     Processing Systems 21 (NIPS). Vancouver (Canada), December.
     #    https://papers.nips.cc/paper/3417-estimation-of-information-theoretic-measures-for-continuous-random-variables.pdf
-    # 
+    #
     #
     r_k = kth_dists
     if kth_dists is None:
@@ -85,20 +85,20 @@ def shannon_entropy_pc(X, k=1, kth_dists=None):
         d = X.shape[1]
     #volume of the unit ball
     v_unit_ball = np.pi**(0.5*d)/gamma(0.5*d + 1.0)
-    # probability estimator using knn distances 
+    # probability estimator using knn distances
     p_k_hat = (k / (n -1.0)) * (1.0/v_unit_ball) * (1.0/r_k**d)
     # log probability
-    log_p_k_hat = np.log(p_k_hat)    
+    log_p_k_hat = np.log(p_k_hat)
     #entropy estimator
     h_k_hat = log_p_k_hat.sum() / (-1.0*n)
     return h_k_hat
- 
+
 #mutual information
 def mutual_information(var_tuple, k=2):
     nvar = len(var_tuple)
     #make sure the input arrays are properly shaped for hstacking
     var_tuple = tuple( var_tuple[i].reshape(len(var_tuple[i]),-1) for i in xrange(nvar) )
-    #compute the individual entropies of each variable  
+    #compute the individual entropies of each variable
     Hx = [shannon_entropy(var_tuple[i],k=k) for i in xrange(nvar)]
     Hx = np.array(Hx)
     # and get the sum
@@ -111,7 +111,7 @@ def mutual_information(var_tuple, k=2):
     #set to zero if value is negative
     if MI < 0.0: MI = 0.0
     #return
-    return MI 
+    return MI
 
 # conditional mutual information
 def conditional_mutual_information(var_tuple, cond_tuple, k=2):
@@ -133,9 +133,9 @@ def conditional_mutual_information(var_tuple, cond_tuple, k=2):
     else:
         Hz = shannon_entropy(cond_tuple[0], k=k)
     Hxzsum = Hxz.sum()
-   # print "Hxzsum: ",Hxzsum, " Hj: ",Hj, " Hz: ",Hz
+    # print "Hxzsum: ",Hxzsum, " Hj: ",Hj, " Hz: ",Hz
     MIc = Hxzsum - Hj - Hz
-   # print "MIc: ",MIc
+    # print "MIc: ",MIc
     if MIc < 0.0: MIc = 0.0
     #print MIc
     return MIc
