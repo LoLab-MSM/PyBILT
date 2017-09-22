04fb14510ccd341aff22daea6200e89981f98249
blakeaw <blakeaw1102@gmail.com>
2017-09-22 12:10:36 -0500

    Finished a version of the Halperin-Nelson metric analysis protocol for bilayer_analyzer and a corresponding test function

---
+++ tests/test_halperin_nelson.py
@@ -0,0 +1,18 @@
+import pybilt.bilayer_analyzer.bilayer_analyzer as ba
+def test_halperin_nelson():
+    analyzer = ba.BilayerAnalyzer(structure='../pybilt/sample_bilayer/sample_bilayer.psf',
+                                  trajectory='../pybilt/sample_bilayer/sample_bilayer_10frames.dcd',
+                                  selection="not resname CLA and not resname TIP3 and not resname POT")
+
+
+    analyzer.remove_analysis('msd_1')
+    analyzer.add_analysis('halperin_nelson hn leaflet upper')
+
+    analyzer.print_analysis_protocol()
+
+    analyzer.run_analysis()
+
+    print analyzer.get_analysis_data('hn')
+
+if __name__ == '__main__':
+    test_halperin_nelson()
