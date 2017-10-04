118a843db14bb7f22be80388f7302368d4365029
blakeaw <blakeaw1102@gmail.com>
2017-10-04 15:22:31 -0500

    Added in docstrings to gaussian module.

--- pybilt/common/gaussian.py
+++ pybilt/common/gaussian.py
@@ -1,9 +1,22 @@
+"""Define Gaussian function objects.
+
+This module defines the Gaussian class and the GaussianRange class.
+
+"""
+
+
 import numpy as np
 
 
 
 class Gaussian(object):
     def __init__(self, mean,std):
+        """Initialize a Gaussian function object.
+        
+        Args:
+            mean (float): Set the mean of the Gaussian. 
+            std (float): Set the standard deviation of the Gaussian. 
+        """
         stdinv = 1.0/std
         normalc = stdinv*(1.0/np.sqrt(np.pi))
         self.sigma = std
@@ -13,6 +26,15 @@ class Gaussian(object):
 
 
     def eval(self,x_in):
+        """Return the Gaussian function evaluated at the input x value. 
+        
+        Args:
+            x_in (float): The x value to evaluate the function at. 
+
+        Returns:
+            float: The function evaluation for the Gaussian.
+
+        """
         stdinv = 1.0/self.sigma
         stdinvsq = stdinv**2
         normalc = self.normconst
@@ -21,12 +43,33 @@ class Gaussian(object):
         return y
 
     def reset_mean(self,new_mean):
+        """Change the mean of the Gaussian function.
+        
+        Args:
+            new_mean (float): The new mean of the Gaussian function. 
+
+        """
         self.mean = new_mean
         return
 
 
 class GaussianRange(object):
     def __init__(self,in_range,mean,std,npoints=200):
+        """Initialize the GaussianRange object.
+        
+        The GaussianRange stores the values of Gaussian function with the 
+        input mean and standard deviation evaluated at evenly spaced points 
+        in the specified x-value range.
+        
+        Args:
+            in_range (tuple, list): Specify the endpoints for range, e.g.
+                (x_start, x_end). 
+            mean (float): The mean of the Gaussian function. 
+            std (float): The standard deviation of the Gaussian function. 
+            npoints (Optional[int]): The number of x-value points to 
+                evaluate the Gaussian function for in the specified range (i.e. 
+                in_range).
+        """
         x_p = np.linspace(in_range[0],in_range[1],npoints,endpoint=True)
         y_p = np.zeros(npoints)
         yc = 0
@@ -50,9 +93,25 @@ class GaussianRange(object):
         return
 
     def get_values(self):
+        """Return the x and y values for the Gaussian range function.
+        
+        Returns:
+            tuple: The x and y values for the function, returned as (
+            x_values, y_values).
+            
+        """
         return (self.x,self.y)
 
     def eval(self,x_in):
+        """Return the Gaussian function evaluated at the input x value. 
+
+        Args:
+            x_in (float): The x value to evaluate the function at. 
+
+        Returns:
+            float: The function evaluation for the Gaussian.
+
+        """
         stdinv = 1.0/self.sigma
         stdinvsq = stdinv**2
         normalc = self.normconst
@@ -61,6 +120,24 @@ class GaussianRange(object):
         return y
 
     def integrate_range(self, lower, upper):
+        """Returns the numerical integration of the Gaussian range.
+        
+        This function does a simple quadrature for the Gaussian function as 
+        evaluated on the range (or subset of the range) specified at 
+        initialization.
+        
+        Args:
+            lower (float): The lower boundary for the integration. 
+            upper (float): The upper boundary for the integration. 
+
+        Returns:
+            float: The numerical value of the Gaussian range integrated from 
+                lower to upper.
+                
+        Notes:
+            This function does not thoroughly check the bounds, so if upper 
+            is less than lower the function will break.
+        """
         if upper>self.upper:
             upper=self.upper
         if lower<self.lower:
@@ -75,6 +152,24 @@ class GaussianRange(object):
         return total
 
     def sum_range(self, lower, upper):
+        """Returns the over the Gaussian range.
+
+        This function sums the Gaussian function at the points that were 
+        evaluated on the range (or subset of the range) specified at 
+        initialization.
+
+        Args:
+            lower (float): The lower boundary for the sum. 
+            upper (float): The upper boundary for the sum. 
+
+        Returns:
+            float: The numerical value of the Gaussian range as summed from 
+                lower to upper.
+
+        Notes:
+            This function does not thoroughly check the bounds, so if upper 
+            is less than lower the function will break.
+        """
         if upper>self.upper:
             upper=self.upper
         if lower<self.lower:
@@ -88,6 +183,7 @@ class GaussianRange(object):
         return total
 
     def normalize(self):
+        """Normalizes (by area) the Gaussian function values over the range."""
         total = 0.0
         for i in xrange(0,self.npoints):
             total+=self.y[i]*self.dx
@@ -96,5 +192,15 @@ class GaussianRange(object):
         return
 
     def reset_mean(self,new_mean):
+        """Change the mean of the Gaussian function.
+
+        Args:
+            new_mean (float): The new mean of the Gaussian function. 
+            
+        Notes:
+            This function does not re-evaluate the Gaussian range and 
+            therefore only affects the output of the eval function.
+
+        """
         self.mean = new_mean
         return
