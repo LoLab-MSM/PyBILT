acdf4193719180dd186da85c8cdeba3c7452bc6d
blakeaw <blakeaw1102@gmail.com>
2017-10-04 21:38:20 -0500

    More  docstrings for lipid grid modules.

--- pybilt/lipid_grid/lipid_grid.py
+++ pybilt/lipid_grid/lipid_grid.py
@@ -1,70 +1,137 @@
-'''
-    Classes and functions to implement lipid COM gridding and analysis for lipid bilayers. Acts on MemSys objects.
-    The gridding and anlaysis procedures are based on
-    the decriptions given in Gapsys et al. J Comput Aided Mol Des (2013) 27:845-858,
-    which is itself a modified version of the GridMAT-MD method by Allen et al. Vol. 30, No. 12 Journal of Computational Chemistry.
-    However, I have currently left out several bits of the extra functionality, e.g. the handling of an embedded protein.
-'''
+"""Build lipid grids derived from COMFrame objects.
+Classes and functions to implement lipid COM gridding and analysis for 
+lipid bilayers. This module defines version that build grids off of 
+COMFrame objects and is meant primarily for internal use by the 
+BilayerAnalyzer class. The gridding and anlaysis procedures are based on
+the descriptions given in Gapsys et al. J Comput Aided Mol Des (2013) 
+27:845-858, which is itself a modified version of the GridMAT-MD method 
+by Allen et al. Vol. 30, No. 12 Journal of Computational Chemistry.
+However, I have currently left out bits of the extra functionality like 
+the handling of an embedded proteins.
+"""
+
+# TODO (blakeaw1102@gmail): Add embedded protein functionality to lipid grid.
 
 import numpy as np
 import sys
 
-#range/xrange fix
-if sys.version_info < (3,0):
-    def range(*args, **kwargs):
-        return xrange(*args, **kwargs)
-#import my running stats class
+# pybilt imports
 from pybilt.common.running_stats import RunningStats
 
-class LipidGrid_2d(object):
-    def __init__(self, com_frame, com_frame_indices,plane,nxbins=50,nybins=50):
-        #store the frame and leaflet
+# range/xrange fix
+if sys.version_info < (3, 0):
+    def range(*args, **kwargs):
+        return xrange(*args, **kwargs)
+
+
+class LipidGrid2d(object):
+    """A 2d lipid grid object.
+    
+    This object is used by the LipidGrids object to construct a 2d grid for 
+    a bilayer leaflet and assign lipids to it using the coordinates derived 
+    from a COMFrame representation object. 
+     
+    Attributes:
+        frame (COMFrame): Stores a local copy of the COMFrame object from 
+            which the the coordinate data and lipid type data is derived from.
+        x_nbins (int): The number of grid bins in the 'x' dimension.
+        y_nbins (int): The number of grid bins in the 'y' dimension.
+        x_min (float): The lower boundary of the grid range in the 'x' 
+            dimension.
+        x_max (float): The upper boundary of the grid range in the 'x' 
+            dimension.
+        x_incr (float): The size of grid boxes (or spacing between grid 
+            points) in the 'x' dimension.
+        x_centers (np.array): The center points of the grid boxes in the 'x'
+            dimension.
+        x_edges (np.array): The edges of the grid boxes in the 'x' dimension.
+        x_centers (np.array): The centers of the grid boxes in the 'x' 
+            dimension.
+        y_min (float): The lower boundary of the grid range in the 'y' 
+            dimension.
+        y_max (float): The upper boundary of the grid range in the 'y' 
+            dimension.
+        y_incr (float): The size of grid boxes (or spacing between grid 
+            points) in the 'y' dimension.
+        y_centers (np.array): The center points of the grid boxes in the 'y'
+            dimension.
+        y_edges (np.array): The edges of the grid boxes in the 'y' dimension.
+        y_centers (np.array): The centers of the grid boxes in the 'y' 
+            dimension.
+        lipid_grid (np.array): A 2d array of size x_nbins*y_nbins that stores 
+            the index of lipids from the COMFrame that are assigned to each 
+            grid box. 
+        lipid_grid_z (np.array): A 2d array of size x_nbins*y_nbins that stores
+            the z coordinate of the lipids assigned to each grid box.
+        
+    """
+    def __init__(self, com_frame, com_frame_indices, plane, nxbins=50,
+                 nybins=50):
+        """Initialize the LipidGrid2d object.
+        
+        Args:
+            com_frame (COMFrame): The instance of COMFrame from which to 
+                pull the coordinates for lipids to use when building the grid. 
+            com_frame_indices (list): A list COMFrame lipid indices to 
+                include when building the grid.
+            plane (list): The indices from the 3d coordinates for the 
+                coordinates that correspond to the bilayer lateral plane. 
+            nxbins (Optional[int]): The number of bins along the 'x' 
+                dimension, i.e. along the dimension corresponding to plane[0]. 
+                Defaults to 50. 
+            nybins (Optional[int): The number of bins along the 'y' 
+                dimension, i.e. along the dimension corresponding to 
+                plane[1]. Defaults to 50.
+        """
+        # store the frame and leaflet
         self.frame = com_frame
-        #self.leaflet = ms_leaflet
-        #get the x and y indices
+        # self.leaflet = ms_leaflet
+        # get the x and y indices
         ix = plane[0]
         iy = plane[1]
-        iz = [i for i in [0,1,2] if i not in plane][0]
-        #get the box dimemsions
+        iz = [i for i in [0, 1, 2] if i not in plane][0]
+        # get the box dimemsions
         box = com_frame.box[plane]
         boxx = box[ix]
         boxy = box[iy]
-        #save the numbers of bins
+        # save the numbers of bins
         self.x_nbins = nxbins
         self.y_nbins = nybins
-        #initialize the edges of the and centers of the gridpoints
+        # initialize the edges of the and centers of the gridpoints
         # x
         self.x_min = 0.0
         self.x_max = boxx
-        self.x_edges = np.linspace(self.x_min,self.x_max,(nxbins+1),endpoint=True)
-        self.x_incr = self.x_edges[1]-self.x_edges[0]
-        x_incr_h = self.x_incr/2.0
+        self.x_edges = np.linspace(self.x_min, self.x_max, (nxbins + 1),
+                                   endpoint=True)
+        self.x_incr = self.x_edges[1] - self.x_edges[0]
+        x_incr_h = self.x_incr / 2.0
         self.x_centers = np.zeros(nxbins)
-        self.x_nedges = len(self.x_edges)
-        for i in range(1,self.x_nedges):
-            j=i-1
-            self.x_centers[j]=self.x_edges[j]+x_incr_h
+        self._x_nedges = len(self.x_edges)
+        for i in range(1, self._x_nedges):
+            j = i-1
+            self.x_centers[j] = self.x_edges[j] + x_incr_h
 
         # y
         self.y_min = 0.0
         self.y_max = boxy
-        self.y_edges = np.linspace(self.y_min,self.y_max,(nybins+1),endpoint=True)
-        self.y_incr = self.y_edges[1]-self.y_edges[0]
-        y_incr_h = self.y_incr/2.0
+        self.y_edges = np.linspace(self.y_min, self.y_max, (nybins + 1),
+                                   endpoint=True)
+        self.y_incr = self.y_edges[1] - self.y_edges[0]
+        y_incr_h = self.y_incr / 2.0
         self.y_centers = np.zeros(nybins)
         self.y_nedges = len(self.y_edges)
-        for i in range(1,self.x_nedges):
-            j=i-1
-            self.y_centers[j]=self.y_edges[j]+y_incr_h
-        self.x_length = self.x_max-self.x_min
-        self.y_length = self.y_max-self.y_min
+        for i in range(1, self._x_nedges):
+            j = i - 1
+            self.y_centers[j] = self.y_edges[j] + y_incr_h
+        self.x_length = self.x_max - self.x_min
+        self.y_length = self.y_max - self.y_min
         # get the lipid indices for this leaflet
         indices = com_frame_indices
-        #now assign lipids to the gridpoints
-        self.lipid_grid = np.zeros((nxbins,nybins),dtype=np.int)
-        self.lipid_grid_z = np.zeros((nxbins,nybins))
-        bxh = boxx/2.0
-        byh = boxy/2.0
+        # now assign lipids to the gridpoints
+        self.lipid_grid = np.zeros((nxbins, nybins), dtype=np.int)
+        self.lipid_grid_z = np.zeros((nxbins, nybins))
+        bxh = boxx / 2.0
+        byh = boxy / 2.0
         cx = 0
         for x in self.x_centers:
             cy = 0
@@ -78,43 +145,80 @@ class LipidGrid_2d(object):
                     yi = com_frame.lipidcom[i].com[iy]
                     zi = com_frame.lipidcom[i].com_unwrap[iz]
                     #print "iz ",iz," zi ",zi
-                    dx = x-xi
-                    dy = y-yi
+                    dx = x - xi
+                    dy = y - yi
                     #Minimum image -- coordinates must be pre-wrapped
                     if np.absolute(dx) > bxh:
-                        dx = boxx - np.absolute(x-bxh) - np.absolute(xi-bxh)
+                        dx = boxx - np.absolute(x - bxh) - np.absolute(xi -
+                                                                       bxh)
                     if np.absolute(dy) > bxh:
-                        dy = boxy - np.absolute(y-byh) - np.absolute(yi-byh)
-                    rxy = np.sqrt(dx**2+dy**2)
+                        dy = boxy - np.absolute(y - byh) - np.absolute(yi -
+                                                                       byh)
+                    rxy = np.sqrt(dx**2 + dy**2)
                     if rxy < r_min:
-                        r_min=rxy
+                        r_min = rxy
                         i_min = i
                         z_min = zi
 
-                self.lipid_grid[cx,cy]=i_min
-                self.lipid_grid_z[cx,cy]=z_min
-                cy+=1
-            cx+=1
+                self.lipid_grid[cx,cy] = i_min
+                self.lipid_grid_z[cx,cy] = z_min
+                cy += 1
+            cx += 1
 
-    def get_index_at(self,ix,iy):
-        return self.lipid_grid[ix,iy]
+    def get_index_at(self, ix, iy):
+        """Returns the COMFrame index of the lipid at the specified position 
+        in the lipid_grid.
+        
+        Args:
+            ix (int): The 'x' index in the lipid_grid. 
+            iy (): The 'y' index in the lipid_grid.
 
-    def get_z_at(self,ix,iy):
-        return self.lipid_grid_z[ix,iy]
+        Returns:
+            int: The index of the lipid.
+
+        """
+        return self.lipid_grid[ix, iy]
+
+    def get_z_at(self, ix, iy):
+        """Returns the z coordinate of the lipid at the specified position 
+        in the lipid_grid.
+
+        Args:
+            ix (int): The 'x' index in the lipid_grid. 
+            iy (): The 'y' index in the lipid_grid.
+
+        Returns:
+            float: The z coordinate of the lipid.
+
+        """
+        return self.lipid_grid_z[ix, iy]
 
     def z_perturb_grid(self):
+        """Returns the array with z coordinates shifted by the mean.
+        
+        Returns:
+            np.array: The mean shifted z coordinate array.
+
+        """
         z_grid = self.lipid_grid_z
         z_avg = z_grid.mean()
         z_pert = z_grid - z_avg
         return z_pert
 
 
-    #Outputs the grid as an xyz coordinate file
+    # Outputs the grid as an xyz coordinate file
     def write_xyz(self, xyz_name):
+        """Write out the lipid grid as an xyz coordinate file.
+        
+        Args:
+            xyz_name (str): File path and name for the output file. 
+            
+
+        """
         # Open up the file to write to
         xyz_out = open(xyz_name, "w")
         npoints = self.x_nbins*self.y_nbins
-        comment = "Leaflet Grid "+self.leaflet.name
+        comment = "Leaflet Grid " + self.leaflet.name
         xyz_out.write(str(npoints))
         xyz_out.write("\n")
         xyz_out.write(comment)
@@ -143,7 +247,7 @@ class LipidGrid_2d(object):
 
 
 class LipidGrids(object):
-    def __init__(self, com_frame, leaflets,plane,nxbins=50,nybins=50):
+    def __init__(self, com_frame, leaflets, plane, nxbins=50, nybins=50):
         #store the frame and leaflet
         self.frame = com_frame
         self.leaflets = leaflets
@@ -156,38 +260,42 @@ class LipidGrids(object):
         #initialize the grids
         #upper
         upper_indices = leaflets['upper'].get_member_indices()
-        self.leaf_grid['upper'] = LipidGrid_2d(com_frame,upper_indices,plane,nxbins=nxbins,nybins=nybins)
+        self.leaf_grid['upper'] = LipidGrid2d(com_frame, upper_indices,
+                                              plane, nxbins=nxbins,
+                                              nybins=nybins)
         #lower
         lower_indices = leaflets['lower'].get_member_indices()
-        self.leaf_grid['lower'] = LipidGrid_2d(com_frame,lower_indices,plane,nxbins=nxbins,nybins=nybins)
+        self.leaf_grid['lower'] = LipidGrid2d(com_frame, lower_indices,
+                                              plane, nxbins=nxbins,
+                                              nybins=nybins)
         return
 
     def thickness_grid(self):
-        tgrid = np.zeros((self.nbins_x,self.nbins_y))
+        tgrid = np.zeros((self.nbins_x, self.nbins_y))
         for ix in range(self.nbins_x):
             for iy in range(self.nbins_y):
-                zu = self.leaf_grid['upper'].get_z_at(ix,iy)
-                zl = self.leaf_grid['lower'].get_z_at(ix,iy)
+                zu = self.leaf_grid['upper'].get_z_at(ix, iy)
+                zl = self.leaf_grid['lower'].get_z_at(ix, iy)
                 dz = zu - zl
-                tgrid[ix,iy]=dz
+                tgrid[ix,iy] = dz
                 if dz < 0.0:
                     print "Warning!!--MD frame number ",self.myframe," --Value thickness less than zero (",dz,") at grid point ",ix," ",iy
         return tgrid
 
-    def average_thickness(self,return_grid=False):
+    def average_thickness(self, return_grid=False):
         trun = RunningStats()
         tgrid = self.thickness_grid()
         for ix in range(self.nbins_x):
             for iy in range(self.nbins_y):
-                tc = tgrid[ix,iy]
+                tc = tgrid[ix, iy]
                 trun.push(tc)
-        avg_out = (trun.mean(),trun.deviation())
+        avg_out = (trun.mean(), trun.deviation())
         if return_grid:
-            return avg_out,tgrid
+            return avg_out, tgrid
         else:
             return avg_out
 
-    def map_to_grid(self,com_values_dict,leaflet='both'):
+    def map_to_grid(self, com_values_dict, leaflet='both'):
         do_leaflet = []
         if leaflet == "both":
             do_leaflet.append('upper')
