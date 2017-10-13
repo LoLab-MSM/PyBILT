5c309ddb6b9161a95f58bf5357497d3ec5bf379a
blakeaw <blakeaw1102@gmail.com>
2017-10-13 11:27:13 -0500

    Updated the version number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pybilt',
-      version='0.0.1',
+      version='v0.1.0',
       description='Lipid bilayer analysis toolkit.',
       author='Blake A. Wilson',
       author_email='blake.a.wilson@vanderbilt.edu',
