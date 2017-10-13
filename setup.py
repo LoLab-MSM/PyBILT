206661977b028811227503677fc37bc6c8a07e16
blakeaw <blakeaw1102@gmail.com>
2017-10-12 19:12:07 -0500

    Added setup.py and updated the rtfd-requirements.txt file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pybilt',
+      version='0.0.1',
+      description='Lipid bilayer analysis toolkit.',
+      author='Blake A. Wilson',
+      author_email='blake.a.wilson@vanderbilt.edu',
+      url='http://pybilt.readthedocs.io/en/latest/index.html',
+      packages=['pybilt'],
+     )
