2c68a7a4d767d875aa333090dcbf3961a4c1bbd4
blakeaw <blakeaw1102@gmail.com>
2019-01-15 18:14:10 -0600

    Updated the run script to now take the second command line argument as the file path to dump the output after the analyzer is finished running.

--- pybilt/bilayer_analyzer/run.py
+++ pybilt/bilayer_analyzer/run.py
@@ -4,14 +4,14 @@ import sys
 import os.path
 
 if __name__ == '__main__':
-    print('goat')
     # get the input script from command line imputs
     input_script = os.path.abspath(sys.argv[1])
+    # get the location to dump the output
+    out_path = os.path.abspath(sys.argv[2])
     # initialize the analyzer with the script
     analyzer = BilayerAnalyzer(input_file=input_script)
     # run the analyzer
     analyzer.run_analysis()
     # dump the output
-    out_path = os.path.abspath('./')
-    # print(out_path)
+    print("dumping output to: " + out_path)
     analyzer.dump_data(path=out_path)
