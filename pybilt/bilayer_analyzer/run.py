4c5ecc0f4551f86d515fc453d82ef9a4a7bd2340
blakeaw <blakeaw1102@gmail.com>
2019-01-15 18:36:28 -0600

    Updated the run script to use argparse for input arguments.

--- pybilt/bilayer_analyzer/run.py
+++ pybilt/bilayer_analyzer/run.py
@@ -4,10 +4,15 @@ import sys
 import os.path
 
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_script', metavar='input_script', type=str, help='The input script with its file path.')
+    parser.add_argument('dump_path', metavar='dump_path', type=str, help='The file path where the analysis outputs should be dumped after the analysis is complete.')
+    args = parser.parse_args()
     # get the input script from command line imputs
-    input_script = os.path.abspath(sys.argv[1])
+    input_script = os.path.abspath(args.input_script)
     # get the location to dump the output
-    out_path = os.path.abspath(sys.argv[2])
+    out_path = os.path.abspath(args.dump_path)
     # initialize the analyzer with the script
     analyzer = BilayerAnalyzer(input_file=input_script)
     # run the analyzer
