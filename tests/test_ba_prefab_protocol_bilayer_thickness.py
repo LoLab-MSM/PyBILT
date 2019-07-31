bf3f91cbf8333480e94f29ded13c4b0c5b71c679
blakeaw <blakeaw1102@gmail.com>
2019-07-30 21:37:47 -0500

    Fixed some bugs in the tests.

--- tests/test_ba_prefab_protocol_bilayer_thickness.py
+++ tests/test_ba_prefab_protocol_bilayer_thickness.py
@@ -1,15 +1,16 @@
-from pybilt.bilayer_analyzer.prefab_analysis_protocols import bilayer_thickness
+from pybilt.bilayer_analyzer.prefab_analysis_protocols import thickness_grid
 
 
 def test_analysis_module_bilayer_thickness():
     sel_string = "resname POPC DOPE TLCL2"
     name_dict = {'DOPE':['P'],'POPC':['P'],'TLCL2':['P1','P3']}
-    bilayer_thickness(structure_file='../pybilt/sample_bilayer/sample_bilayer.psf',
-                  trajectory_file='../pybilt/sample_bilayer/sample_bilayer_10frames.dcd',
-                  selection_string=sel_string,
-                  name_dict=name_dict,
-                  frame_start=0, frame_end=-2, frame_interval=2,
-                  n_xbins=100, n_ybins=100)
+    thickness_grid(structure_file='../pybilt/sample_bilayer/sample_bilayer.psf',
+                   trajectory_file='../pybilt/sample_bilayer/sample_bilayer_10frames.dcd',
+                   selection_string=sel_string,
+                   name_dict=name_dict,
+                   frame_start=0, frame_end=-2, frame_interval=2,
+                   n_xbins=100, n_ybins=100,
+                   plot_grid_map=False)
 
     return
 
