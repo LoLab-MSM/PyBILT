ea21743bf30126c803d11298f2143320c6833aff
blakeaw <blakeaw1102@gmail.com>
2019-09-16 15:11:33 -0500

    Fixed some issues in the test_lipid_grid_surface_area test module.

--- tests/test_lipid_grid_surface_area.py
+++ tests/test_lipid_grid_surface_area.py
@@ -1,13 +1,9 @@
 from __future__ import print_function
 from pybilt.bilayer_analyzer import BilayerAnalyzer
 import numpy as np
-from pybilt.plot_generation import plot_generation_functions as pgf
-from pybilt.plot_generation.plot_generation_functions import _color_list
-from scipy.ndimage.filters import gaussian_filter
 import scipy.constants as scicon
 
 def test_lipid_grid_surface_area():
-    sel_string = "resname POPC DOPE TLCL2"
     name_dict = {'DOPE':['P'],'POPC':['P'],'TLCL2':['P1','P3']}
     analyzer = BilayerAnalyzer(structure='../pybilt/sample_bilayer/sample_bilayer.psf',
                                   trajectory='../pybilt/sample_bilayer/sample_bilayer_10frames.dcd',
@@ -20,8 +16,6 @@ def test_lipid_grid_surface_area():
     analyzer.rep_settings['lipid_grid']['n_xbins'] = nbins
     analyzer.rep_settings['lipid_grid']['n_ybins'] = nbins
     analyzer.set_frame_range(0,1,1)
-    i =0
-    zgrid = None
     for _frame in analyzer:
         sa_upper, sa_lower = analyzer.reps['lipid_grid'].surface_area(filter_sigma=5.0)
         print(("surface for upper leaflet: {} lower leaflet {}".format(sa_upper, sa_lower)))
